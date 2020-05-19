dfbfeeb84b9aa129929030609d0224f0f506e45e
robertpreseau <robertpreseau@gmail.com>
2020-05-18 20:13:29 -0400

    added gganimate plot

--- antm.r
+++ antm.r
@@ -1,7 +1,7 @@
 ## STANDARD SETUP #########################################################################
 
 #Create list of required packages
-list.of.packages <- c('purrr', 'dplyr', 'rvest', 'stringr')
+list.of.packages <- c('purrr', 'dplyr', 'rvest', 'stringr', 'ggplot2', 'gganimate', 'gifski')
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 
 #Install missing packages
@@ -12,6 +12,9 @@ library('dplyr')
 library('purrr')
 library('rvest')
 library('stringr')
+library('ggplot2')
+library('gganimate')
+library('gifski')
 
 
 #Clear out environment variables
@@ -39,15 +42,44 @@ webpage <- read_html(url)
 
 #Using this tutorial https://www.freecodecamp.org/news/an-introduction-to-web-scraping-using-r-40284110c848/
 #extract the HTML element we want to retrieve data from
-table <- html_table(html_nodes(webpage, xpath='//*[@id="mw-content-text"]/div/table[3]'))
+table <- html_table(html_nodes(webpage, xpath='//*[@id="mw-content-text"]/div/table[5]'), fill=TRUE)
 
-#Pull out useful columns
-contestant <- as.character(data.frame(table[1])[,1])
-finish <- as.integer(data.frame(table[1])[,6])
+#finishing order
+finishing_order <- as.data.frame(data.frame(table[1])[-1,3])
+finishing_order$week <- 1
+finishing_order$callout <- as.numeric(rownames(finishing_order))
+names(finishing_order) <- c('contestant', 'week', 'callout')
+
+i <- 4
+while (i < 12) {
+  finishing_order_tmp <- as.data.frame(data.frame(table[1])[-1,i])
+  finishing_order_tmp$week <- i-2
+  finishing_order_tmp$callout <- as.numeric(rownames(finishing_order_tmp))
+  names(finishing_order_tmp) <- c('contestant', 'week', 'callout')
+
+  finishing_order <- rbind(finishing_order, finishing_order_tmp)
+  
+  i <- i + 1
+}  
 
 
-#Create dataframe from intermediate columns
-results <- data.frame(contestant, finish)
+finishing_order <- finishing_order %>% filter(contestant != '')
+
+#source cleanup
+rm(table, webpage, i, url)
 
 
-results
+
+# Plot
+p <- finishing_order %>%
+  ggplot(aes(x=week, y=callout, group=contestant, color=contestant)) +
+  geom_line() +
+  theme(legend.position = "none") +
+  geom_text(aes(label=contestant),hjust=0, vjust=0) +
+  ggtitle("America's Next Top Model - Season 1 Weekly Finishing Order") +
+  ylab("Weekly Callout Position")  +
+  scale_x_continuous(breaks = unique(finishing_order$week), limits=c(0, 9), expand=expand_scale(mult=c(0,.2), add=c(0, 0))) + 
+  scale_y_continuous(trans = "reverse", breaks = unique(finishing_order$callout)) +
+  transition_reveal(week)
+
+animate(p, end_pause=30)
